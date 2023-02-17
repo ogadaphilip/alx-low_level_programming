06de00a1a0b247d73d51ca35567e98e4568015ad
ogadaphilip <ogadaphilip6@gmail.com>
2023-02-17 04:39:49 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * Main - We are going to write a C program that prints a line given below
+ * Return : 0
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle")
+return (0);
+}
